d6a3df01ea981f8aa9004c11a182cef56f32c185
jonklinger <jonklinger.github@2jk.org>
2015-05-29 23:54:18 +0300

    Create startcoin.sh

---
+++ startcoin.sh
@@ -0,0 +1,13 @@
+date >> /home/pi/.bitcoin/running.log
+
+if [ "$(pidof bitcoind)" ]
+then
+   echo sync at block >> /home/pi/.bitcoin/running.log
+	/usr/local/bin/bitcoind getblockcount >> /home/pi/.bitcoin/running.log
+
+else
+
+	/usr/local/bin/bitcoind -dns -noupnp -maxconnections=12 -timeout=120 -noirc -gen=0 -maxorphantx=15 -maxorphanblocks=15 -dbcache=5 -daemon -checkblocks=25 -maxreceivebuffer=1250 -maxsendbuffer=250 -disablewallet &
+	echo was dead >> /home/pi/.bitcoin/running.log
+
+  fi
