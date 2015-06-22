806dfa59b778e79f6c8bc2c1273b31eb2fd186b2
Jonathan Klinger <jonklinger.github@2jk.org>
2015-06-22 14:16:21 +0300

    Update startcoin.sh

--- startcoin.sh
+++ startcoin.sh
@@ -8,7 +8,7 @@ then
 
 else
 
-	/usr/local/bin/bitcoind -dns -noupnp -maxconnections=12 -timeout=120 -noirc -gen=0 -maxorphantx=15 -maxorphanblocks=15 -dbcache=5 -daemon -checkblocks=25 -maxreceivebuffer=1250 -maxsendbuffer=250 -disablewallet &
+	/usr/local/bin/bitcoind -dns -noupnp -maxconnections=24 -timeout=120 -noirc -gen=0 -maxorphantx=15 -maxorphanblocks=15 -dbcache=15 -daemon -checkblocks=25 -maxreceivebuffer=1250 -maxsendbuffer=250 -disablewallet &
 	echo was dead >> /home/pi/.bitcoin/running.log
 
   fi
